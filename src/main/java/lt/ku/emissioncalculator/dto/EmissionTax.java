79419009a4c30627574c76e016064aec18aaf507
Dovydas Oginskas <oginskasd@gmail.com>
2023-05-10 20:11:13 +0300

    added max validation

--- src/main/java/lt/ku/emissioncalculator/dto/EmissionTax.java
+++ src/main/java/lt/ku/emissioncalculator/dto/EmissionTax.java
@@ -1,5 +1,6 @@
 package lt.ku.emissioncalculator.dto;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -10,6 +11,7 @@ public class EmissionTax {
 
     @NotNull(message = "Šis laukelis yra privalomas.")
     @Min(value = 1, message = "Minimali laukelio reikšmė turi būti 1.")
+    @Max(value = 1000, message = "Laukelio reikšmė negali būti didesnė nei 1000.")
     private Integer emission;
 
     public Integer getEmission() {
